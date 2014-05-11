8f5da5f344e1ea2b5b74909e157fda96e0539a88
Stephan Miller <stephanmil@gmail.com>
2014-05-11 04:09:58 -0500

    edits

--- provision/shell/install_php.sh
+++ provision/shell/install_php.sh
@@ -1,2 +1,2 @@
 echo '### Install PHP ###'
-sudo apt-get install -y php5 php-pear php5-dev php5-mysql libpcre3-dev zlib1g-dev libaio1 git
+sudo apt-get install -y php5 php-pear php5-mcrypt php5-dev php5-mysql php-mbstring libpcre3-dev zlib1g-dev libaio1 git
