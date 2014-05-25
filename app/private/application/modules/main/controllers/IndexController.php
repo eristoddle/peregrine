65d80487ec564c50b9ef22bba9ba3d129130bfcb
Stephan Miller <stephanmil@gmail.com>
2014-05-24 23:42:40 -0500

    more trimming

--- app/private/application/modules/main/controllers/IndexController.php
+++ app/private/application/modules/main/controllers/IndexController.php
@@ -6,15 +6,9 @@ use \Peregrine\Main\Controllers\ModuleController;
 
 /**
  * Concrete implementation of Main module controller
- *
- * @RoutePrefix("/main/api")
  */
 class IndexController extends ModuleController {
-    /**
-     * @Route("/index", paths={module="main"}, methods={"GET"}, name="main-index-index")
-     */
     public function indexAction() {
-        $this->view->setVar('page', 'HI HOW ARE YOU');
-        //var_dump($this->view);
+
     }
 }
